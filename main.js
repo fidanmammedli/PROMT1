ed09788e7fe3f5b601138e81d601261dd5b29af9
Fidan <146016464+fidanmammedli@users.noreply.github.com>
2023-12-06 21:32:54 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let emailin = document.getElementById("email");
+
+if(emailin !== "" ){
+    alert("E-mail movcuddur");
+}
+else{
+    alert("E-mail yazilamyib");
+}
+let password = document.getElementById("kod").value;
+
+if(password !== "" ){
+    alert("Sifre movcuddur");
+}
+else{
+    alert("Sifre yazilamyib");
+}
